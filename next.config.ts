d018ad381b432d7fa38f7c24c14367441bec2bbe
samani21 <eksloba21@gmail.com>
2026-08-08 14:48:48 +0800

    Update npm run build gagal

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['192.168.1.128', '192.168.1.9'],
 };
 
 export default nextConfig;
